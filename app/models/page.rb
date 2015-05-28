dd6a4c7b614423222ef110b208aaf862ad3890fa
Pat <patjhorsley@gmail.com>
2015-05-29 10:08:26 +1200

    Slug created from title if not specified

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,8 +1,18 @@
 class Page < ActiveRecord::Base
   custom_slugs_with(:slug)
 
+  before_save :parameterize_slug
+  
   validates :title, presence: true
   validates :content, presence: true
-  validates :slug, uniqueness: true
+  validates :slug, uniqueness: true 
+    # format: { with: /\A^[A-z\-\s]+\z/, message: "only allows letters and hyphen (-)" }
+
+  private
+
+  def parameterize_slug
+    self.slug = self.title if self.slug.empty?
+    self.slug = self.slug.downcase.parameterize
+  end
 
 end

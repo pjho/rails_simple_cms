ad6bfbccde3a9286aa682e9009c28b2f9a5e5a8a
Pat <patjhorsley@gmail.com>
2015-06-04 11:04:52 +1200

    add pagenation to post indexes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,6 @@ class PostsController < ApplicationController
   end
   
   def index
-    @posts = Post.where( published: true )
+    @posts = Post.where( published: true ).order('created_at DESC').paginate(:page => params[:page], :per_page => 10)
   end
 end

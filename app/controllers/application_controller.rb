eb25792c3cc7cd5b138eb85844bbf4d71ee0cca9
Pat <patjhorsley@gmail.com>
2015-06-04 15:09:48 +1200

    re-auth required to add admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,6 +24,13 @@ class ApplicationController < ActionController::Base
     cms_path
   end
 
+
+  def reAuthCurrentAdmin(pass)
+    Admin.find(current_admin.id).valid_password?(pass)
+  end
+
+
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
--- app/controllers/cms/admins_controller.rb
+++ app/controllers/cms/admins_controller.rb
@@ -11,10 +11,15 @@ class Cms::AdminsController < CmsController
 
   def create
     @admin = Admin.new(admin_params)
-    if @admin.save
+
+    # Check current admin has entered their correct password 
+    reAuth = reAuthCurrentAdmin(params['admin']['current_admin_pass'])
+
+    if reAuth && @admin.save
       flash[:notice] = "Successfully created Admin." 
       redirect_to cms_admins_path
     else
+      flash[:alert] = "You need to enter your current password to make changes." unless reAuth
       render :action => 'new'
     end
   end
@@ -25,46 +30,25 @@ class Cms::AdminsController < CmsController
   end
 
   def update
-
     @admin = Admin.find(params[:id])
     # Only allow sudo admins to edit other admins.
     authenticateSuper! unless current_admin.id == @admin.id 
 
     # Check current admin has entered their correct password 
-    authCurrentAdmin = Admin.find(current_admin.id).valid_password?(params['admin']['current_admin_pass'])
+    reAuth = reAuthCurrentAdmin(params['admin']['current_admin_pass'])
     
-    Rails.logger.debug("Admin: #{authCurrentAdmin.inspect}")
-
+    # Filter password parameters of not set
     params[:admin].delete(:password) if params[:admin][:password].blank?
     params[:admin].delete(:password_confirmation) if params[:admin][:password].blank? and params[:admin][:password_confirmation].blank?
     
-    if !authCurrentAdmin
-      flash[:alert] = "You need to enter your current password to make changes."
-      render :action => 'edit'
-    elsif @admin.update_attributes(admin_params)
+    if reAuth && @admin.update_attributes(admin_params)
       flash[:notice] = "Successfully updated Admin."
       redirect_to edit_cms_admin_path
     else
+      flash[:alert] = "You need to enter your current password to make changes." unless reAuth
       render :action => 'edit'
     end
   end
-
-  # def updateasdsadsad
-  #  @user = User.find(params[:id])
-  #  user = User.find_by_email(current_user.email).try(:authenticate, params[:current_admin_pass])
-  #   if user && @user.update_attributes(params[:user])
-  #     flash[:success] = "Profile updated"
-  #     sign_in @user
-  #     redirect_to @user
-  #   else
-  #     flash.now[:error] = "Incorrect Current Password" unless user
-  #     sign_in @user
-  #     render 'edit'
-  #   end
-  # end
-
-
-
 
 
   def destroy

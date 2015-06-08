096bbf2a64b05e8b9718f86eef34b51fdaa0cba8
Pat <patjhorsley@gmail.com>
2015-06-08 12:52:04 +1200

    added class method for generating tag css. also added pre_validation callback.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,10 +1,22 @@
 class Tag < ActiveRecord::Base
+# after_save { Tag::generateTagCss }
+before_validation :upcase_color
 
-has_many :taggings
+has_many :taggings, dependent: :destroy
 has_many :posts, through: :taggings
 
 validates :name, presence: true
 validates :color, format: { with: /\A#[A-F0-9]{6}+\z/,
     message: "Color must be in the format #ABC123" }
+
+private
+  def self.generateTagCss
+    TagCssGenerator.generate
+  end
+
+
+  def upcase_color
+    self.color = self.color.upcase
+  end
 
 end
